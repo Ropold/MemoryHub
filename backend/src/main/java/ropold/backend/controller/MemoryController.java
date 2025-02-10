f579d5335c3ef5a8e6db20c8cf3cebad239b3299
Robert Stolz <robert.stolz87@gmail.com>
2025-02-10 09:49:54 +0100

    cloudinary

--- backend/src/main/java/ropold/backend/controller/MemoryController.java
+++ backend/src/main/java/ropold/backend/controller/MemoryController.java
@@ -1,11 +1,15 @@
 package ropold.backend.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ropold.backend.model.MemoryModel;
 import ropold.backend.model.MemoryModelDto;
 import ropold.backend.service.MemoryService;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -14,14 +18,34 @@ import java.util.List;
 public class MemoryController {
 
     private final MemoryService memoryService;
+    private final CloudinaryService cloudinaryService;
 
     @GetMapping()
     public List<MemoryModel> getAllMemories() {
         return memoryService.getAllMemories();
     }
 
+    @GetMapping("/active")
+    public List<MemoryModel> getActiveMemories() {
+        return memoryService.getActiveMemories();
+    }
+
+    @GetMapping("/{id}")
+    public MemoryModel getMemoryById(@PathVariable String id) {
+        return memoryService.getMemoryById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping()
-    public MemoryModel addMemory(@RequestBody MemoryModelDto memoryModelDto) {
+    public MemoryModel addMemory(
+            @RequestPart("memoryModelDto") @Valid MemoryModelDto memoryModelDto,
+            @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
+
+        String imageUrl = null;
+        if (image != null && !image.isEmpty()) {
+            imageUrl = cloudinaryService.uploadImage(image);
+        }
+
         return memoryService.addMemory(
                 new MemoryModel(
                         null,
