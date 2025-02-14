463b5b718e666569f6adbc2e67feb4cc28987a90
Robert Stolz <robert.stolz87@gmail.com>
2025-02-14 13:25:10 +0100

    first frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from "./components/Footer.tsx";
+import Navbar from "./components/Navbar.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>MemoryHub</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <h2>App</h2>
+      <Footer />
     </>
   )
 }
 
-export default App
+
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h2>Navbar</h2>
+            <button>Button</button>
+        </nav>
+    );
+}
