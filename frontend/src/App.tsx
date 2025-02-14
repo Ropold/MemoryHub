ce71f9b6a450a9ece0d8e65c33bbf3e50170fdcb
Robert Stolz <robert.stolz87@gmail.com>
2025-02-14 13:36:42 +0100

    first frontend 2nd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,34 @@
 import './App.css'
 import Footer from "./components/Footer.tsx";
 import Navbar from "./components/Navbar.tsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function App() {
 
+    const [user, setUser] = useState<string>("anonymousUser");
+
+
+    function getUser() {
+        axios.get("/api/users/me")
+            .then((response) => {
+                setUser(response.data.toString());
+            })
+            .catch((error) => {
+                console.error(error);
+                setUser("anonymousUser");
+            });
+    }
+
+    useEffect(() => {
+        getUser();
+    }, []);
+
   return (
     <>
-      <Navbar />
+      <Navbar
+        user={user}
+      />
       <h2>App</h2>
       <Footer />
     </>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,13 @@
 
-export default function Navbar() {
+type NavbarProps = {
+    user: string
+}
+
+export default function Navbar(props: Readonly<NavbarProps>) {
     return (
         <nav>
             <h2>Navbar</h2>
+            <p>User: {props.user}</p>
             <button>Button</button>
         </nav>
     );
